5aa9355b927d518b56a834d630697b3e3f8813dc
Michal Kváček <michal.kvacek@performax.cz>
2023-08-31 09:29:30 +0200

    Razeni letu podle vzletu

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,18 @@ DOCKER_REGISTRY="docker.kvacek.cz"
 CONTAINER_IMAGE="$DOCKER_REGISTRY/$IMAGE_NAME:$TAG"
 
 docker login $DOCKER_REGISTRY
-
 docker build -t $CONTAINER_IMAGE .
 docker push $CONTAINER_IMAGE
+
+# TODO: nahravat i config pro apache?
+
+
+scp docker-compose.prod.yml michal@kvacek.cz:/var/www/poletani.cz/api/docker-compose.yml
+
+ssh -t michal@kvacek.cz  << EOF
+  cd /var/www/poletani.cz/api
+  docker-compose pull
+  docker-compose down --remove-orphans
+  docker-compose up -d
+  docker-compose exec -T api alembic upgrade head
+EOF
