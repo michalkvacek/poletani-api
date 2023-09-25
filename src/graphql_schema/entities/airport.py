dc49cd347c5cde2b0ca1236b16d73bcdfbc2dde9
Michal Kváček <michal.kvacek@performax.cz>
2023-09-25 10:07:56 +0200

    Pouziti base resolveru u letiste

--- src/graphql_schema/entities/airport.py
+++ src/graphql_schema/entities/airport.py
@@ -2,8 +2,7 @@ from typing import List
 import strawberry
 from database import models
 from decorators.endpoints import authenticated_user_only
-from graphql_schema.entities.resolvers.airport import get_airport_resolver
-from graphql_schema.entities.resolvers.base import get_list, get_one
+from graphql_schema.entities.resolvers.base import get_list, get_one, get_base_resolver
 from graphql_schema.sqlalchemy_to_strawberry_type import strawberry_sqlalchemy_type
 
 
@@ -17,11 +16,11 @@ class AirportQueries:
     @strawberry.field()
     @authenticated_user_only()
     async def airports(root, info) -> List[Airport]:
-        query = get_airport_resolver(info.context.user_id)
+        query = get_base_resolver(models.Airport, user_id=info.context.user_id)
         return await get_list(models.Airport, query)
 
     @strawberry.field()
     @authenticated_user_only()
     async def airport(root, info, id: int) -> Airport:
-        query = get_airport_resolver(info.context.user_id, id)
+        query = get_base_resolver(models.Airport, user_id=info.context.user_id, object_id=id)
         return await get_one(models.Airport, query)
